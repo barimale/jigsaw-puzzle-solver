330cbf387fe820519afef9152f0b52dba3f73e0f
Mateusz <mateusz.wolnica@gmail.com>
2023-02-26 16:21:38 +0100

    .

--- Genetic.Algorithm.Tangram.Solver.Logic.UT/As_A_Developer/I_would_like_to_solve_simple_board.cs
+++ Genetic.Algorithm.Tangram.Solver.Logic.UT/As_A_Developer/I_would_like_to_solve_simple_board.cs
@@ -1,3 +1,5 @@
+using Genetic.Algorithm.Tangram.Solver.Logic.GameParts.Board;
+
 namespace Genetic.Algorithm.Tangram.Solver.Logic.UT.As_A_Developer
 {
     public class I_would_like_to_solve_simple_board
@@ -17,10 +19,25 @@ namespace Genetic.Algorithm.Tangram.Solver.Logic.UT.As_A_Developer
         public void With_shape_of_2x2_fields_by_using_3_blocks_and_two_types_of_fields()
         {
             // given
+            var scaleFactor = 1;
+            var fields = new List<BoardFieldDefinition>()
+            {
+                new BoardFieldDefinition(0,0, true, scaleFactor),
+                new BoardFieldDefinition(0,1, true, scaleFactor),
+                new BoardFieldDefinition(1,0, true, scaleFactor),
+                new BoardFieldDefinition(1,1, true, scaleFactor)
+            };
+            BoardShapeBase boardDefinition = new BoardShapeBase(fields, 2, 2, 1);
+            //board
+            //blocks
+            var blocks = [];
+            //solver
 
             // when
+            //execute solver
 
             // then
+            // all suitable solutions are displayed in console
         }
     }
 }
---
+++ Genetic.Algorithm.Tangram.Solver.Logic.UT/Utilities/DataHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace Genetic.Algorithm.Tangram.Solver.Logic.UT.Utilities
+{
+    public static class DataHelper
+    {
+        // TODO: move data definition from unit test here
+    }
+}
