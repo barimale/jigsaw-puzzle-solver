10712a8c67907caf9bf59f32881ff23076d32299
Mateusz <mateusz.wolnica@gmail.com>
2023-03-02 00:37:15 +0100

    .

--- Genetic.Algorithm.Tangram.Solver.Logic/TangramMutation.cs
+++ Genetic.Algorithm.Tangram.Solver.Logic/TangramMutation.cs
@@ -13,15 +13,8 @@ namespace Genetic.Algorithm.Tangram.Solver.Logic
 
         protected override void PerformMutate(IChromosome chromosome, float probability)
         {
-            var prob = RandomizationProvider.Current.GetFloat(0, 1);
-
-            if( prob <= probability)
-            {
-                var mutated = (TangramChromosome)chromosome.CreateNew();
-
-                // TODO: maybe specific or random genes only
-                chromosome.ReplaceGenes(0, mutated.GetGenes());
-            }
+            var mutated = (TangramChromosome)chromosome.CreateNew();
+            chromosome.ReplaceGenes(0, mutated.GetGenes());
         }
     }
 }
