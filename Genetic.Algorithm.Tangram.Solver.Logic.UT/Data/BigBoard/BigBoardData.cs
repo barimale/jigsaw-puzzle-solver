894f1c99d2e49eaec4d22e58265da9a1a940200e
Mateusz <mateusz.wolnica@gmail.com>
2023-03-01 23:52:44 +0100

    .

--- Genetic.Algorithm.Tangram.Solver.Logic.UT/Data/BigBoard/BigBoardData.cs
+++ Genetic.Algorithm.Tangram.Solver.Logic.UT/Data/BigBoard/BigBoardData.cs
@@ -64,7 +64,7 @@ namespace Genetic.Algorithm.Tangram.Solver.Logic.UT.Data.BigBoard
             // solver
             // and check everything once again
             var generationChromosomesNumber = 1000;
-            var mutationProbability = 0.2f;
+            var mutationProbability = 0.05f;
             var crossoverProbability = 1.0f - mutationProbability;
             var chromosome = new TangramChromosome(
                 blocks,
