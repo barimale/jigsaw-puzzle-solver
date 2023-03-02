bc5f4335380ee4b0ef79860f84933015a72e3303
Mateusz <mateusz.wolnica@gmail.com>
2023-03-02 06:42:09 +0100

    .

--- Genetic.Algorithm.Tangram.Solver.Logic/GameParts/GamePartsConfigurator.cs
+++ Genetic.Algorithm.Tangram.Solver.Logic/GameParts/GamePartsConfigurator.cs
@@ -6,6 +6,13 @@ namespace Genetic.Algorithm.Tangram.Solver.Logic.GameParts
 {
     public class GamePartsConfigurator
     {
+        // TODO: provide the allowedLocations generator for each block 
+        // random only specific geometry, so just one int
+        // implement thread safe randomizer
+        // and use tplOperator together with multithreading where is possible
+        // refactor first 
+        // think how many times execute the algorithm,
+        // maybe the reinformcent then time termination
         public BoardShapeBase Board { private set; get; }
         public IList<BlockBase> Blocks { private set; get; }
         public int[] AllowedAngles { get; private set; }
