5817e6bec95ad829594e6c7d8a68733c9d1f70f7
Mateusz <mateusz.wolnica@gmail.com>
2023-03-21 17:36:33 +0100

    .

---
+++ Genetic.Algorithm.Tangram.Solver.Logic.UT/UTs/Algorithms/TreeSearches/FindQuickestSearchAlgorithm/DepthFirst_for_medium_board_needs_to_be_tested.cs
@@ -0,0 +1,51 @@
+using Xunit.Abstractions;
+using Assert = Xunit.Assert;
+using Genetic.Algorithm.Tangram.Solver.Logic.UT.BaseUT;
+using Genetic.Algorithm.Tangram.Solver.Logic.UT.Helpers;
+using Algorithm.Tangram.TreeSearch.Logic;
+using Solver.Tangram.Game.Logic;
+using Solver.Tangram.AlgorithmDefinitions.Generics;
+
+namespace Genetic.Algorithm.Tangram.Solver.Logic.UT.UTs.Algorithms.TreeSearches.FindQuickestSearchAlgorithm
+{
+    public class DepthFirst_for_medium_board_needs_to_be_tested : PrintToConsoleUTBase
+    {
+        private AlgorithmUTConsoleHelper AlgorithmUTConsoleHelper;
+
+        public DepthFirst_for_medium_board_needs_to_be_tested(ITestOutputHelper output)
+            : base(output)
+        {
+            AlgorithmUTConsoleHelper = new AlgorithmUTConsoleHelper(output);
+        }
+
+        [Fact]
+        public async Task Containing_4_blocks_with_X_and_O_markups_and_5x4_board_with_0_and_1_fields()
+        {
+            // given
+            var gameParts = GameBuilder
+                .AvalaibleGameSets
+                .CreatePolishMediumBoard(withAllowedLocations: true);
+
+            var algorithm = GameBuilder
+                .AvalaibleTSTemplatesAlgorithms
+                .CreateDepthFirstTreeSearchAlgorithm(
+                    gameParts.Board,
+                    gameParts.Blocks);
+
+            var game = new GameBuilder()
+                .WithGamePartsConfigurator(gameParts)
+                .WithAlgorithm(algorithm)
+                .Build();
+
+            // when
+            var result = await game.RunGameAsync<AlgorithmResult>();
+
+            // then
+            Assert.NotNull(result);
+
+            // finally
+            Display("DepthFirst");
+            AlgorithmUTConsoleHelper.ShowMadeChoices(result.GetSolution<FindFittestSolution>());
+        }
+    }
+}
--- TODOs.md
+++ TODOs.md
@@ -1,23 +1,8 @@
-// TODO: add n times for GA UTs
 
 	add mtcs with UCB1 from GameAI
 
 	hide as many classes as possible -> so composition once again	
 
-// refactor naming convention for gamepartsconfigurator
-// modify its builder methods
-// to the form of LINQ maybe?
 // use the IsEnabled field of the board field when allowed locations
 // are obtained -> Genius Junior
 
-// choosing the algorithm depending on the amount of blocks
-// <= 5 GA, more TS or parallel or support for parallel and by default selection
-// depending on the amount of blocks
-// so the calculationStrategy -> single or array, with mode whenall/first
-// at the end add the MCTS from GameAI lib
-
-// maybe at the end modify the game configurator
-// to the form of LINQ as already done here:
-```
-https://github.com/rvhuang/linq-to-astar
-```
