b6250d6116ffccb738bd579d568ad99fa204fcb5
Mateusz <mateusz.wolnica@gmail.com>
2023-03-27 00:09:10 +0200

    .

--- wpfchrometabs-mvvm/Demo/ViewModel/TabClass2.cs
+++ wpfchrometabs-mvvm/Demo/ViewModel/TabClass2.cs
@@ -1,9 +1,20 @@
-﻿namespace Demo.ViewModel
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Demo.ViewModel
 {
+    // game parts: board and blocks
    public class TabClass2 : TabBase
     {
         public string MyStringContent { get; set; }
         public int[] MyNumberCollection { get; set; }
         public int MySelectedNumber { get; set; }
+        public object MyCanvasContent { get; set; } = new Canvas()
+        {
+            Background = new SolidColorBrush()
+            {
+                Color = Colors.DarkGreen
+            }
+        };
     }
 }
