f853327065d42700d9b6354c764d1ad3f69ebf23
Mateusz <mateusz.wolnica@gmail.com>
2023-03-26 01:06:13 +0100

    .

--- Genetic.Algorithm.Tangram.Solver/MainWindow.xaml.cs
+++ Genetic.Algorithm.Tangram.Solver/MainWindow.xaml.cs
@@ -94,11 +94,11 @@ namespace Genetic.Algorithm.Tangram.Solver
             {
                 var gameParts = GameBuilder
                     .AvalaibleGameSets
-                    .CreatePolishMediumBoard(withAllowedLocations: true);
+                    .CreatePolishBigBoard(withAllowedLocations: true);
 
                 var algorithm = GameBuilder
                     .AvalaibleTSTemplatesAlgorithms
-                    .CreateDepthFirstTreeSearchAlgorithm(
+                    .CreatePilotTreeSearchAlgorithm(
                         gameParts.Board,
                         gameParts.Blocks);
                 //gameParts.AllowedAngles);
