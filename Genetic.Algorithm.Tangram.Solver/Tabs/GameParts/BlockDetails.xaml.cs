ec8ff060754f51c5d9b96cbba840fa1165ccbdc2
Mateusz <mateusz.wolnica@gmail.com>
2023-03-26 21:20:15 +0200

    .

---
+++ Genetic.Algorithm.Tangram.Solver/Tabs/GameParts/BlockDetails.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Algorithm.Executor.WPF.Tabs
+{
+    /// <summary>
+    /// Interaction logic for GamePartsOverview.xaml
+    /// </summary>
+    public partial class BlockDetails : UserControl
+    {
+        public BlockDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Genetic.Algorithm.Tangram.Solver/Tabs/GameParts/GamePartsOverview.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Algorithm.Executor.WPF.Tabs
+{
+    /// <summary>
+    /// Interaction logic for GamePartsOverview.xaml
+    /// </summary>
+    public partial class GamePartsOverview : UserControl
+    {
+        public GamePartsOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
