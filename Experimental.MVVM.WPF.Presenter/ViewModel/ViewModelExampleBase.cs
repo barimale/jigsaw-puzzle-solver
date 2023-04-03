3284049df66c733a1f87a0a0755f65fb9dba5b2f
Mateusz <mateusz.wolnica@gmail.com>
2023-04-04 01:23:46 +0200

    .

--- Experimental.MVVM.WPF.Presenter/ViewModel/ViewModelExampleBase.cs
+++ Experimental.MVVM.WPF.Presenter/ViewModel/ViewModelExampleBase.cs
@@ -66,7 +66,7 @@ namespace Demo.ViewModel
         {
             var gameParts = GameBuilder
                 .AvalaibleGameSets
-                .CreatePolishMediumBoard(withAllowedLocations: true);
+                .CreatePolishBigBoard(withAllowedLocations: true);
 
             var oneRootTS = GameBuilder
                 .AvalaibleTSTemplatesAlgorithms
@@ -83,7 +83,7 @@ namespace Demo.ViewModel
 
             var ga = GameBuilder
                 .AvalaibleGATunedAlgorithms
-                .CreateMediumBoardSettings(
+                .CreateBigBoardSettings(
                     gameParts.Board,
                     gameParts.Blocks,
                     gameParts.AllowedAngles);
