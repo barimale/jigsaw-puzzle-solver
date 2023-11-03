9e1086a15711b756a8cc8343942db272c2d92c4c
Mateusz <mateusz.wolnica@gmail.com>
2023-11-03 12:41:15 +0100

    .

---
+++ Christmas.Secret.Gifter.Domain/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using TypeGen.Core.TypeAnnotations;
+
+namespace Christmas.Secret.Gifter.Domain
+{
+    [ExportTsInterface]
+    public class GameSettings
+    {
+        public string Id { get; set; } = null!;
+        public IEnumerable<AlgorithmDetails > AlgorithmDetails { get; set; } = new List<AlgorithmDetails>();
+        public GamePartsDetails GamePartsDetails { get; set; } = null!;
+    }
+}
