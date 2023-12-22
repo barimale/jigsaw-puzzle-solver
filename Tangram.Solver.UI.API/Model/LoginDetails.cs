9193117c42c1240f174a3eebbd422f0e36886391
barimale <mateusz.wolnica@gmail.com>
2023-12-22 15:50:25 +0100

    Delete Tangram.Solver.UI.API directory

--- Tangram.Solver.UI.API/Model/LoginDetails.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tangram.Solver.UI.Model
-{
-    public class LoginDetails
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- Tangram.Solver.UI.API/Services/Abstractions/IAuthorizeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Tangram.Solver.UI.Services.Abstractions
-{
-    public interface IAuthorizeService
-    {
-        string GetToken(IdentityUser user);
-    }
-}
