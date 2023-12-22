e47466ef315df9dae4e6aa1f557dd355258bbb6f
barimale <mateusz.wolnica@gmail.com>
2023-12-22 15:50:47 +0100

    Delete Tangram.Solver.UI.Database.SQLite directory

--- Tangram.Solver.UI.Database.SQLite/Repositories/Abstractions/IEventRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tangram.Solver.UI.Database.SQLite.Entries;
-
-namespace Tangram.Solver.UI.Database.SQLite.Repositories.Abstractions
-{
-    public interface IEventRepository : IBaseRepository<EventEntry, string>
-    {
-        //intentionally left blank
-    }
-}
