8f138c9b67d263e91e9ee5a035c42efecfa0263a
Mateusz <mateusz.wolnica@gmail.com>
2025-02-26 17:42:25 +0100

    .

--- Tangram.Common.GameParts.Configurator/Generics/MultiAlgorithms/MultiAlgorithm.cs
+++ Tangram.Common.GameParts.Configurator/Generics/MultiAlgorithms/MultiAlgorithm.cs
@@ -6,70 +6,56 @@ namespace Solver.Tangram.AlgorithmDefinitions.Generics.MultiAlgorithms
 {
     public class MultiAlgorithm : IExecutableMultiAlgorithm
     {
-        private readonly Dictionary<string, IExecutableAlgorithm> algorithms = new Dictionary<string, IExecutableAlgorithm>();
+        private readonly ImmutableDictionary<string, IExecutableAlgorithm> algorithms;
         private readonly ExecutionMode executionMode;
 
         public MultiAlgorithm(
             ExecutionMode executionMode,
             IList<IExecutableAlgorithm> algorithms)
         {
+            if (algorithms == null) throw new ArgumentNullException(nameof(algorithms));
+
             this.executionMode = executionMode;
-            algorithms.ToList().ForEach(p =>
-            {
-                this.algorithms.TryAdd(p.Id, p);
-            });
+            this.algorithms = algorithms.ToImmutableDictionary(p => p.Id, p => p);
         }
 
         public MultiAlgorithm(
             ExecutionMode executionMode,
             params IExecutableAlgorithm[] algorithms)
         {
+            if (algorithms == null) throw new ArgumentNullException(nameof(algorithms));
+
             this.executionMode = executionMode;
-            algorithms.ToList().ForEach(p =>
-            {
-                this.algorithms.TryAdd(Guid.NewGuid().ToString(), p);
-            });
+            this.algorithms = algorithms.ToImmutableDictionary(p => Guid.NewGuid().ToString(), p => p);
         }
 
-        public Dictionary<string, IExecutableAlgorithm> Algorithms => algorithms;
+        public ImmutableDictionary<string, IExecutableAlgorithm> Algorithms => algorithms;
 
-        public event EventHandler<SourceEventArgs> QualityCallback; // of func / action here
+        public event EventHandler<SourceEventArgs> QualityCallback;
         public event EventHandler OnExecutionEstimationReady;
 
         public async Task<AlgorithmResult[]> ExecuteManyAsync(CancellationToken ct = default)
         {
-
             ct.ThrowIfCancellationRequested();
 
             var allOfThem = algorithms
-                    .Keys
-                    .Select(pp =>
-                    {
-                        algorithms[pp].QualityCallback += QualityCallback;
-                        algorithms[pp].OnExecutionEstimationReady += OnExecutionEstimationReady;
-
-                        return algorithms[pp];
-                    })
-                    .Select(p => Task
-                            .Run(async () => await p.ExecuteAsync(ct), ct))
-                    .ToImmutableArray();
+                .Values
+                .Select(p =>
+                {
+                    p.QualityCallback += QualityCallback;
+                    p.OnExecutionEstimationReady += OnExecutionEstimationReady;
+                    return p.ExecuteAsync(ct);
+                })
+                .ToImmutableArray();
 
             switch (executionMode)
             {
                 case ExecutionMode.WhenAll:
-                    var results = await Task
-                        .WhenAll(allOfThem);
-
-                    return results
-                        .ToArray();
+                    var results = await Task.WhenAll(allOfThem);
+                    return results.ToArray();
                 case ExecutionMode.WhenAny:
-                    var result = await Task
-                        .WhenAny(allOfThem);
-
-                    return new AlgorithmResult[]
-                    {
-                        result.Result
-                    };
+                    var result = await Task.WhenAny(allOfThem);
+                    return new AlgorithmResult[] { await result };
                 default:
                     throw new ArgumentException("ExecutionMode not recognized");
             }
